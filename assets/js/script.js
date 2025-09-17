d108fab4df507f2596f8cda0724786781e44b319
IRCF <ircfspace@gmail.com>
2025-09-17 09:45:36 +0330

    smallFix

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,8 +43,8 @@ function renderData(dataArray) {
       x++;
     }
   });
+  let loadMore = '<div class="clearfix"></div>';
   if (dataArray.length > 15) {
-    let loadMore = '<div class="clearfix"></div>';
     loadMore +=
       '<a class="btn btn-warning btn-block" href="https://github.com/ircfspace/endpoint/blob/main/ip.json" dir="rtl" target="_blank">';
     loadMore += "مشاهده بیشتر";
